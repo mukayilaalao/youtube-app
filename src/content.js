42097bb06b5109620370ab9e16f5ca39fa4aba97
Lakisha Trusty <daprincessanime@gmail.com>
2021-12-06 16:02:07 -0500

    done the fetch

---
+++ src/content.js
@@ -0,0 +1,6 @@
+fetch("https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=arianagrande&type=video&key=AIzaSyCJuTI4g41QrymV8Sxa7wFTWQdUidWyCcY")
+    .then((res)=>{
+        return res.json();
+    }).then((data)=>{
+        
+    })
