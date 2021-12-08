0f7168364073d6ab9afead5caa8b37a07a6b15b8
Jenna Qian <jiayin1996@gmail.com>
2021-12-08 18:23:17 -0500

    Change NavBar from class to functional component

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,13 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css"
 
-class NavBar extends React.Component {
-    
-
-    render() {
-        return (
-            <div className="navbar">
-                <img src="https://www.freeiconspng.com/thumbs/youtube-logo-png/logo-youtube-png-27.png" alt="Youtube Logo"/>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-            </div>
-        )
-    }
+function NavBar () {
+    return (
+        <div className="navbar">
+            <img src="https://www.freeiconspng.com/thumbs/youtube-logo-png/logo-youtube-png-27.png" alt="Youtube Logo"/>
+            <Link className="icon" to="/"><p>Home</p></Link>
+            <Link className="icon" to="/about"><p>About</p></Link>
+        </div>
+    )
 }
-
 export default NavBar;
